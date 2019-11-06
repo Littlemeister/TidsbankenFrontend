96beb58c9ddf1dc459f06d823d6f6cc642c70f66
Andreas Poulsen <andreas.peter.poulsen@gmail.com>
2019-11-06 12:45:37 +0100

    merge

--- tidsbanken-frontend/src/components/login/LoginComponent.tsx
+++ tidsbanken-frontend/src/components/login/LoginComponent.tsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import styles from '../../css/login.module.css';
+import axios from 'axios';
+import { Http2ServerRequest } from 'http2';
+import { Redirect } from 'react-router-dom';
 
 type MyProps = {
 
 }
 
 type MyState = {
-    email: any,
-    password: any
+    email: string,
+    password: string,
+    error: boolean,
+    message: string,
+    loggin: boolean
 }
 
 export default class LoginComponent extends React.Component<MyProps, MyState>{
 
     state: MyState = {
         email: "",
-        password: ""
+        password: "",
+        error: false,
+        message: "Error",
+        loggin: false
     }
 
-    handleSubmit = (event:any) => {
-        //console.log("something was submittet: " + this.state.value);
+    handleSubmit = (event:any) => {       
         event.preventDefault();
+        console.log("something was submittet: " + this.state.email + this.state.password);
+        this.serverRequest();
     }
 
     handleChange = (event:any): void => {
@@ -31,18 +41,35 @@ export default class LoginComponent extends React.Component<MyProps, MyState>{
         
     }
 
+    serverRequest = () =>{
+        // axios.get(`https://localhost/data`)
+        // .then(res => {
+        //      this.setState({
+        //             loggin:true
+        //         });
+        // })
+        this.setState({
+            loggin:true
+        });
+    }
+
     render(){
-        return(
-            <div id={styles.login_wrapper}>
-                <h1>LOGIN</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label> <p>Email</p> <input name="email" type="email" placeholder="Enter your email" onChange={this.handleChange} value={this.state.email}/> </label>
-                    <label> <p>Password</p> <input name="password" type="password" placeholder="Enter your password" onChange={this.handleChange} value={this.state.password} /> </label>
-                    <label><button id={styles.submit_login} type="submit">Submit</button> </label>
-                    <button id={styles.forgot_password}>forgot password</button>
-                </form>
-            </div>
-        )
+        if(this.state.loggin){
+            return <Redirect to='/2fa' /> 
+        } else {
+            return(
+                <div id={styles.login_wrapper}>
+                    <h1>LOGIN</h1>
+                    <form onSubmit={this.handleSubmit}>
+                        <label> <p>Email</p> <input name="email" type="email" placeholder="Enter your email" onChange={this.handleChange} value={this.state.email}/> </label>
+                        <label> <p>Password</p> <input name="password" type="password" placeholder="Enter your password" onChange={this.handleChange} value={this.state.password} /> </label>
+                        <p id={styles.errorMessage}>{this.state.error && this.state.message}</p>
+                        <label><button id={styles.submit_login} type="submit">Submit</button> </label>
+                        <button id={styles.forgot_password} type="button">forgot password</button>
+                    </form>
+                </div>
+            )
+        }
     } // END render()
 
 }
