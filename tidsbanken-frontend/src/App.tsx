f7fe325741e7a8c3639adfd0d1c7064eef7a0874
Victor Heijler <victor.heijler@se.experis.com>
2019-11-06 12:42:27 +0100

    Remove Auth from wrapping all of the component, we should instead do it per view, to ensure that not all views are hidden behind auth

--- tidsbanken-frontend/src/App.tsx
+++ tidsbanken-frontend/src/App.tsx
@@ -9,23 +9,20 @@ import Header from './components/common/header/Header';
 import ViewNotFound from './views/common/ViewNotFound';
 import MyView from './views/common/MyView';
 import Login from './views/login/Login';
-import Auth from './components/auth/Auth';
 
 const App: React.FC = () => {
     return (
         <div className="App">
-            <Auth>
-                <Router>
-                    <Header />
-                    <Switch>
-                        <Route exact path="/" component={Login} />
-                        <Route path="/login" component={Login} />
-                        <Route path="/2fa" component={MyView} />
-                        <Route path="/test" component={MyView} />
-                        <Route component={ViewNotFound} />
-                    </Switch>
-                </Router>
-            </Auth>
+            <Router>
+                <Header />
+                <Switch>
+                    <Route exact path="/" component={Login} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/2fa" component={MyView} />
+                    <Route path="/test" component={MyView} />
+                    <Route component={ViewNotFound} />
+                </Switch>
+            </Router>
         </div>
     );
 }
