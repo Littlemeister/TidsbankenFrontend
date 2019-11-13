04b151f0cead8a4a289054913f29024134e22126
Victor Heijler <victor.heijler@se.experis.com>
2019-11-13 16:09:32 +0100

    Rewrite TwoFactorAuthComponent to be a functional component

--- tidsbanken-frontend/src/components/login/TwoFactorAuthComponent.tsx
+++ tidsbanken-frontend/src/components/login/TwoFactorAuthComponent.tsx
@@ -1,69 +1,81 @@
-import React from 'react';
+import React, { useState, useContext, useRef, useEffect } from 'react';
 import styles from '../../css/TwoFactorAuth.module.css';
 import commonStyles from '../../css/Common.module.css';
-// import axios from 'axios';
+import axios from 'axios';
 import { Redirect } from 'react-router-dom';
+import AuthContext from '../auth/AuthContext';
 
-type MyProps = {
+const TwoFactorAuthComponent = (props: any) => {
+    const auth = useContext(AuthContext);
+    let inputRef = useRef<HTMLInputElement>(null);
 
-}
+    const [token, setToken] = useState("");
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [message, setMessage] = useState("");
+    const [disabled, setDisabled] = useState(true);
 
-type MyState = {
-    token: string,
-    success: boolean,
-    error: boolean,
-    message: string,
-}
+    useEffect(() => {
+        (inputRef.current as any).focus();
+    }, [])
 
-export default class TwoFactorAuthComponent extends React.Component<MyProps, MyState>{
-
-    state: MyState = {
-        token: "",
-        success: false,
-        error: false,
-        message: "",
-    }
-
-    handleSubmit = (event:any) => {       
-        event.preventDefault();
-        this.serverRequest();
-    }
-
-    handleChange = (event:any): void => {
-        this.setState({
-            [event.target.name]: event.target.value
-        } as MyState);
-    }
-
-    serverRequest = () =>{
-        this.setState({
-            success: true
+    const login2fa = () => {
+        return axios(`${process.env.REACT_APP_API_URL}/login2fa`, {
+            method: "POST",
+            withCredentials: true,
+            data: { "password2fa": token }
         });
     }
 
-    render(){
-        if(this.state.success){
-            return <Redirect to='/2fa' /> 
-        } else {
-            return(
-                <div className={styles.module}>
-                    <h1>TWO FACTOR AUTHENTICATION</h1>
-                    <form onSubmit={this.handleSubmit}>
-                        <label className={commonStyles.label} htmlFor="token">Token</label>
-                        <input 
-                            name="token" 
-                            className={commonStyles.input} 
-                            type="text" 
-                            placeholder="Token" 
-                            onChange={this.handleChange} 
-                            value={this.state.token}
-                        />
-                        <p id={styles.errorMessage}>{this.state.error && this.state.message}</p>
-                        <button className={commonStyles.button} type="submit">Authenticate</button>
-                    </form>
-                </div>
-            )
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        try {
+            let login = await login2fa();
+            if (login.status === 200) {
+                auth.setUser(login.data);
+                console.log(auth);
+                setSuccess(true);
+            }
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                setError(true);
+            }
         }
     }
 
+    const handleChange = (event: any) => {
+        setToken(event.target.value);
+        if(event.target.value.length >= 5) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
+    }
+
+    return (
+        <>
+            {success ? <Redirect to="/dashboard" /> : ""}
+            <div className={styles.module}>
+                <h1>TWO FACTOR AUTHENTICATION</h1>
+                <form onSubmit={handleSubmit}>
+                    <label className={commonStyles.label} htmlFor="token">Token</label>
+                    <input
+                        name="token"
+                        className={commonStyles.input}
+                        type="text"
+                        placeholder="Token"
+                        onChange={handleChange}
+                        value={token}
+                        minLength={5}
+                        maxLength={6}
+                        ref={inputRef}
+                    />
+                    <p id={styles.errorMessage}>{error && message}</p>
+                    <button className={commonStyles.button} type="submit" disabled={disabled}>Authenticate</button>
+                </form>
+            </div>
+        </>
+    )
 }
+
+export default TwoFactorAuthComponent;
