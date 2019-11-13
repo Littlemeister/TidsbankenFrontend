4f0269f19a8280a9b81d7787556cdb9ceedd5c7f
Victor Heijler <victor.heijler@se.experis.com>
2019-11-13 10:06:47 +0100

    Make modal dismissable with escape and also adjust css to not use a large margin to center the modal

--- tidsbanken-frontend/src/components/common/modal/Modal.tsx
+++ tidsbanken-frontend/src/components/common/modal/Modal.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../../../css/Modal.module.css';
 
 const Modal = (props: any) => {
+
+    useEffect(() => {
+        document.addEventListener("keydown", hide);
+        return (() => document.removeEventListener("keydown", hide));
+    }, [props.display]);
+
+    const hide = (event: KeyboardEvent) => {
+        if (event.keyCode === 27) {
+            props.setDisplay(false);
+        }
+    }
+
     if (props.display) {
         return (
             <div className={styles.module} onClick={event => props.setDisplay(false)}>
