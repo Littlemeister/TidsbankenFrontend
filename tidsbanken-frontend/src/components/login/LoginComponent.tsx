10628bb8078ac124033f9f5d3210533ff299498e
Victor Heijler <victor.heijler@se.experis.com>
2019-11-13 16:08:47 +0100

    Convert the LoginComponent to be a functional component instead of a class component.

--- tidsbanken-frontend/src/components/login/LoginComponent.tsx
+++ tidsbanken-frontend/src/components/login/LoginComponent.tsx
@@ -1,168 +1,118 @@
-import React from 'react';
+import React, { useState, useEffect, useContext, useRef } from 'react';
 import styles from '../../css/Login.module.css';
 import commonStyles from '../../css/Common.module.css';
 import axios from 'axios';
 import { Redirect } from 'react-router-dom';
 import Popover from '../common/popover/Popover';
+import AuthContext from '../auth/AuthContext';
 
-type MyProps = { }
-
-type MyState = {
-    email: string,
-    password: string,
-    error: boolean,
-    message: string,
-    success: boolean,
-    popOver: boolean, 
-    ls_timeLeft: number,
-    ls_timeTo: number,
-    minutLeft: number,
-    secondsLeft: number,
-    btnNotDisabled: boolean
-}
-
-export default class LoginComponent extends React.Component<MyProps, MyState> {
-
-    state: MyState = {
+const LoginComponent = (props: any) => {
+    const auth = useContext(AuthContext);
+    let inputRef = useRef<HTMLInputElement>(null);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [message, setMessage] = useState("");
+    const [btnDisabled, setBtnDisabled] = useState(false);
+    const [input, setInput] = useState({
         email: "",
-        password: "",
+        password: ""
+    });
+    const [ls, setLs] = useState({
+        minutesLeft: 0,
+        secondsLeft: 0,
+    });
 
-        // If login succeded or not
-        success: false,
+    useEffect(() => {
+        getTimeLeft();
+        (inputRef.current as any).focus(); 
+    }, []);
 
-        // Forgot password
-        popOver: false, 
+    useEffect(() => {
+        console.log(auth);
+    }, [auth])
 
-        // Error handling
-        error: false,
-        message: "",
+    useEffect(() => {
+        setMessage(`You have made 5 attempts please try again in ${ls.minutesLeft} minutes and ${ls.secondsLeft} seconds`);
+    }, [ls])
 
-        // Time handling
-        ls_timeLeft: 0,        // used for localstorage
-        ls_timeTo: 0,          // used for localstorage
-        minutLeft: 0,       // used for frontend
-        secondsLeft: 0,      // used for frontend
-        btnNotDisabled: true //Disable btn so user's cant send login requests
+    const timeLeft = (): number => {
+        return Number(localStorage.getItem("timeTo")) - Date.now();
     }
 
-    componentDidMount = () => {
-        this.getTimeLeft(); // Check if a localstorage timeTo is set which indicates that a failed login attempt has been made. It also sets a state that has the amount of time left until a loginattempt can be made again.
-    }
-
-    checkCounter = () => {
-        // Set counter to 5 minuts in localstorage and state "ls_timeLeft" if there is none or zero present
-        if(localStorage.getItem("timeTo") === "" || localStorage.getItem("timeTo") === null || localStorage.getItem("timeTo") === undefined){
-            this.setCounterInLocalStorage(); // Set counter in localStorage
-            this.timer(); //Start timer for frontend
-            this.setState({btnNotDisabled:false})
-        } else { // If counter in localstorage already exsist and is not zero - update the counter
-            this.getTimeLeft();
-        }
-    }
-
-    // Sets localstorage AND state to a date that is 5 minuts from now
-    setCounterInLocalStorage = () => {
-        const timeNow = new Date().getTime();
-        const timeTo = (new Date().getTime() + (5*60000));
-        var distance = timeTo - timeNow;
-        var minutes = this.getMinuts(distance);
-
-        localStorage.setItem("timeTo", timeTo.toString());
-    }
-
-    getTimeLeft = () => {
-        if(localStorage.getItem("timeTo")){
-            const to = Number(localStorage.getItem("timeTo"));
-            const now = (new Date().getTime());
-            const timeDistance = to - now;
-            console.log("this.state.secondsLeft: ");
-            console.log(this.state.secondsLeft);
-            
-
-            if(timeDistance > 0){
-                this.timer();
-                this.setState({
-                    ls_timeLeft: timeDistance,
-                    btnNotDisabled:false,
-                    error: true,
-                });  
-            } else { 
-                this.setState({
-                    ls_timeLeft: 0,
-                    btnNotDisabled:true
-                });
+    const timer = () => {
+        let timer: number = window.setInterval(() => {
+            let timeDelta: number = timeLeft();
+            if (timeDelta < 0) {
+                window.clearInterval(timer);
+                setError(false);
+                setMessage("");
                 localStorage.removeItem("timeTo");
-                console.log("timeTo in Localstorage was removed");
-            }   
-        } else {
-            console.log("No localstorage timeTo found");
-        }
-    }
-
-    getMinuts(distance:any){
-        return Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    }
-
-    timer(){
-        // Update the count down every 1 second
-        var counter = setInterval(() => {
-            var now = new Date().getTime();  // Get today's date and time
-            var distance = Number(localStorage.getItem("timeTo")) - now; // Find the distance between now and the count down date
-        
-            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            var secondsLeft = distance - (minutes * 60);
-            console.log("minute " + minutes);
-            console.log("secondsleft " + seconds);
-
-            if (distance < 0) {
-                clearInterval(counter);
+                setBtnDisabled(false);
             } else {
-                this.setState({
-                    minutLeft: minutes,       // used for frontend
-                    secondsLeft: seconds,      // used for frontend
-                    message: "You have made 5 attempts please try again in " + this.state.minutLeft + " minutes and " + this.state.secondsLeft + " seconds",
-                })
-                console.log(this.state.secondsLeft);
-            }          
-
+                let minutes = Math.floor((timeDelta % (1000 * 60 * 60)) / (1000 * 60));
+                let seconds = Math.floor((timeDelta % (1000 * 60)) / 1000);
+                setLs({
+                    ...ls,
+                    minutesLeft: minutes,
+                    secondsLeft: seconds,
+                });
+                setMessage(`You have made 5 attempts please try again in ${ls.minutesLeft} minutes and ${ls.secondsLeft} seconds`);
+            }
         }, 1000);
     }
 
-    handleSubmit = async (event: any) => {
-        event.preventDefault();
-        try {
-            let login = await this.login();
-            console.log(login);
-            if (login.status === 200) {
-                this.setState({ success: true });
-            }
-        } catch (error) {
-            this.setState({ error: true });
-            console.log(error.response.data['numOfAttemptedLogins']);
-            if(error.response.data.hasOwnProperty('timeOut') || error.response.data['numOfAttemptedLogins'] === 5){ 
-                this.setState({ 
-                    message: "You have made 5 attempts please try again in " + this.state.secondsLeft + " minutes" 
-                });
-                this.checkCounter(); // Set counter or update counter from localstorage
+    const getTimeLeft = () => {
+        if (localStorage.getItem("timeTo")) {
+            if (timeLeft() > 0) {
+                timer();
+                setBtnDisabled(true);
+                setError(true);
             } else {
-                this.setState({ 
-                    message: "You have " + (5 - error.response.data['numOfAttemptedLogins']) + " attempts left." 
-                })
+                setBtnDisabled(false);
+                localStorage.removeItem("timeTo");
             }
-            console.log(error.response.data);
         }
     }
 
-    handleChange = (event: any): void => {
-        this.setState({
-            [event.target.name]: event.target.value
-        } as MyState);
+    const setCounterInLocalStorage = () => {
+        const timeTo = new Date().getTime() + (5 * 60000);
+        localStorage.setItem("timeTo", timeTo.toString());
     }
 
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        try {
+            let response = await login();
+            if (response.status === 200) {
+                auth.setUser(response.data);
+                setSuccess(true);
+            }
+        } catch (error) {
+            if (error.response.status === 401 || error.response.status === 504) {
+                setError(true);
+                let errorData = error.response.data;
+                if (errorData.hasOwnProperty("timeOut") || errorData['numOfAttemptedLogins'] === 5) {
+                    setMessage(`You have made 5 attempts, please try again in ${ls.minutesLeft} minutes`)
+                    setCounterInLocalStorage();
+                    timer();
+                    setBtnDisabled(true);
+                } else {
+                    setMessage(`You have ${(5 - errorData['numOfAttemptedLogins'])} attempts left`);
+                }
+            }
+            // If TwoFactorAuthentication
+            if (error.response.status === 418) {
+                setSuccess(true);
+            }
+        }
+    }
 
-    login = () => {
-        const { email, password } = this.state;
+    const handleChange = (event: any) => {
+        setInput({ ...input, [event.target.name]: event.target.value });
+    }
+
+    const login = () => {
+        const { email, password } = input;
         return axios(`${process.env.REACT_APP_API_URL}/login`, {
             method: "POST",
             withCredentials: true,
@@ -170,23 +120,21 @@ export default class LoginComponent extends React.Component<MyProps, MyState> {
         });
     }
 
-    render() {
-
-        if (this.state.success) {
-            return <Redirect to='/2fa' />
-        } else {
-            return (
+    return (
+        <>
+            {success ? <Redirect to="/2fa" /> :
                 <div id={styles.login_wrapper}>
                     <h1>LOGIN</h1>
-                    <form onSubmit={this.handleSubmit}>
+                    <form onSubmit={handleSubmit}>
                         <label className={commonStyles.label} htmlFor="email">Email</label>
                         <input
                             name="email"
                             className={commonStyles.input}
                             type="email"
                             placeholder="Enter your email"
-                            onChange={this.handleChange}
-                            value={this.state.email}
+                            onChange={handleChange}
+                            value={input.email}
+                            ref={inputRef}
                         />
                         <label className={commonStyles.label} htmlFor="password">Password</label>
                         <input
@@ -194,25 +142,25 @@ export default class LoginComponent extends React.Component<MyProps, MyState> {
                             className={commonStyles.input}
                             type="password"
                             placeholder="Enter your password"
-                            onChange={this.handleChange}
-                            value={this.state.password}
+                            onChange={handleChange}
+                            value={input.password}
                         />
-                        <p id={styles.errorMessage}>{this.state.error && this.state.message}</p>
+                        <p id={styles.errorMessage}>{error && message}</p>
 
-                        <button className={commonStyles.button + " " + styles.submit_login} type="submit" disabled={!this.state.btnNotDisabled}>Login</button>
+                        <button className={commonStyles.button + " " + styles.submit_login} type="submit" disabled={btnDisabled}>Login</button>
 
-                        <Popover 
+                        <Popover
                             trigger="Forgot password?"
                             triggerId={styles.forgot_password}
                             id={styles.popOver}
                         >
                             Please contact your administrator for a new password
                         </Popover>
-
                     </form>
                 </div>
-            )
-        }
-    } // END render()
-
+            }
+        </>
+    )
 }
+
+export default LoginComponent;
