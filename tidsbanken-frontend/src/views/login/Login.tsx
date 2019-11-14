30c719bde22ef0b8b6e1f15fae8b58187ec1bd58
Victor Heijler <victor.heijler@se.experis.com>
2019-11-14 09:15:22 +0100

    Fix merge conflic

--- tidsbanken-frontend/src/views/login/Login.tsx
+++ tidsbanken-frontend/src/views/login/Login.tsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import LoginComponent from '../../components/login/LoginComponent';
 
-<<<<<<< HEAD
-export default class Login extends React.Component{
-    render(){
-        return(
-            <LoginComponent />
-        )
-    }
-}
-=======
 const Login = () => <LoginComponent />
 
 export default Login;
->>>>>>> f92b6eef5646b45845479040acb58a0042c57c99
