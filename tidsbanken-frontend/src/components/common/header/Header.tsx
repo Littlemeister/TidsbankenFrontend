9a85adb14868ae6f59115a0bdc4eed968c8e9965
Victor Heijler <victor.heijler@se.experis.com>
2019-11-14 11:12:09 +0100

    Conditionally render header based on user status and role

--- tidsbanken-frontend/src/components/common/header/Header.tsx
+++ tidsbanken-frontend/src/components/common/header/Header.tsx
@@ -9,63 +9,87 @@ import Pusher from 'pusher-js';
 const Header = (props: any) => {
 
     const { user } = useContext(AuthContext);
-    var [liArray, setLiArray] = useState<any[]>([]);    // Used for li html elemnts
-    const [update, setUpdate] = useState<any>({});      // Used for li html elemnts
+    const [liArray, setLiArray] = useState<any[]>([]);    // Used for li html elements
+    const [update, setUpdate] = useState<any>({});      // Used for li html elements
 
     // Setup push notification
-    var pusher = new Pusher('4c6550e4e866a013a371', {
+    const pusher = new Pusher('4c6550e4e866a013a371', {
         cluster: 'eu',
         forceTLS: true
     });
 
     useEffect(() => {
-        var channel = pusher.subscribe('notifications');
+        let channel = pusher.subscribe('notifications');
 
-        channel.bind('user_update', function(data: any) {
+        channel.bind('user_update', function (data: any) {
             setUpdate(data);
         });
 
-        channel.bind('pusher:subscription_succeeded', function(members) {
+        channel.bind('pusher:subscription_succeeded', function (members) {
             console.log('successfully subscribed! - Pusher');
         });
     }, [])
 
     useEffect(() => {
         getNotification();
-    },[update]);
+    }, [update]);
 
-    function getNotification(){
+    function getNotification() {
         // DONT FORGET TO UPDATE USERID CONTROLLEN !!!!!)!&)!(/%!!)
         // DONT FORGET TO UPDATE USERID CONTROLLEN !!!!!)!&)!(/%!!)
         // DONT FORGET TO UPDATE USERID CONTROLLEN !!!!!)!&)!(/%!!)
         // DONT FORGET TO UPDATE USERID CONTROLLEN !!!!!)!&)!(/%!!)
-        if(update.userId){
+        if (update.userId) {
             const liElement = <li className={update.status ? "good" : "bad"}> {update.userId}</li>
             setLiArray(liArray.concat(liElement));
-        }  
+        }
     }
+
+    const loggedIn = user && user.hasOwnProperty("name") && user.hasOwnProperty("isAdmin") && user.isAdmin === 0;
+    const loggedInAdmin = user && user.hasOwnProperty("isAdmin") && user.isAdmin === 1;
+    const loggedOut = Object.entries(user as object).length === 0 && (user as object).constructor === Object;
 
     return (
         <header className={styles.module}>
-                <Link to="/">Home</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/2fa">2FA</Link>
-                <Link to="/test">Test</Link>
+            {loggedIn && <>
                 <Link to="/dashboard">Dashboard</Link>
-
-                <Dropdown title={ (user && user.name) || "Menu" }>
-                    <ul className={commonStyles.dropdown}>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/2fa">2fa</Link></li>
-                        <li><Link to="/logout">Log out</Link></li>
-                    </ul>
-                </Dropdown>
-
                 <Dropdown title={'Notifications'}>
                     <ul>{liArray}</ul>
                 </Dropdown>
-            </header>
+                <Dropdown title={(user && user.name) || "Menu"}>
+                    <ul className={commonStyles.dropdown}>
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li><Link to="/logout">Logout</Link></li>
+                    </ul>
+                </Dropdown>
+            </>}
+            {loggedInAdmin && <>
+                <Link to="/dashboard">Dashboard</Link>
+                <Link to="/admin">Admin</Link>
+                <Dropdown title={'Notifications'}>
+                    <ul>{liArray}</ul>
+                </Dropdown>
+                <Dropdown title={(user && user.name) || "Menu"}>
+                    <ul className={commonStyles.dropdown}>
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li><Link to="/logout">Logout</Link></li>
+                    </ul>
+                </Dropdown>
+            </>}
+            {loggedOut && <>
+                <Link to="/login">Login</Link>
+            </>}
+            <Dropdown title={"All routes"}>
+                <ul className={commonStyles.dropdown}>
+                    <li><Link to="/">/</Link></li>
+                    <li><Link to="/login">Login</Link></li>
+                    <li><Link to="/2fa">2FA</Link></li>
+                    <li><Link to="/test">Test</Link></li>
+                    <li><Link to="/dashboard">Dashboard</Link></li>
+                    <li><Link to="/logout">Logout</Link></li>
+                </ul>
+            </Dropdown>
+        </header>
     )
 }
 
